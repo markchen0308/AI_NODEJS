6f05c1da67cd8b265f3c77bda13ba3949b8ea59e
markchen0308 <simonchen0308@gmail.com>
2018-09-21 18:51:44 +0800

    ai

--- test/dist/test02.js
+++ test/dist/test02.js
@@ -11,4 +11,10 @@ let a = tf.tensor([1.0, 2.0, 3.0, 10.0, 20.0, 30.0], shape);
 a.print(); // print Tensor values
 let b = tf.tensor([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
 b.print();
+let c = tf.tensor2d([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
+c.print();
+tf.scalar(3.14).print();
+tf.tensor1d([1, 2, 3]).print();
+tf.tensor2d([[1, 2], [3, 4]]).print();
+tf.tensor3d([[[1], [2]], [[3], [4]]]).print();
 //# sourceMappingURL=test02.js.map
--- test/src/test02.ts
+++ test/src/test02.ts
@@ -15,3 +15,11 @@ a.print(); // print Tensor values
 
 let b :tf.Tensor<tf.Rank>= tf.tensor([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
 b.print();
+
+let c:tf.Tensor<tf.Rank.R2> = tf.tensor2d([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
+c.print();
+
+tf.scalar(3.14).print();
+tf.tensor1d([1, 2, 3]).print();
+tf.tensor2d([[1, 2], [3, 4]]).print();
+tf.tensor3d([[[1], [2]], [[3], [4]]]).print();
